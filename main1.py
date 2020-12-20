9cef6c442fe437e873117dc71d6c99db1fced34a
soham <sohampatkar93@gmail.com>
2020-12-20 19:57:24 +0530

    trail commit

--- main1.py
+++ main1.py
@@ -32,18 +32,27 @@ app.config['MQTT_TLS_CA_CERTS'] = CA
 app.config['MQTT_TLS_CERTFILE'] = CERTI
 app.config['MQTT_TLS_KEYFILE'] = KEYFILE
 
+# app.config['MQTT_BROKER_URL'] = 'broker.hivemq.com' # use the free broker from HIVEMQ
+# app.config['MQTT_BROKER_PORT'] = 1883 # default port for non-tls connection
+# app.config['MQTT_USERNAME'] = '' # set the username here if you need authentication for the broker
+# app.config['MQTT_PASSWORD'] = '' # set the password here if the broker demands authentication
+# app.config['MQTT_KEEPALIVE'] = 5 # set the time interval for sending a ping to the broker to 5 seconds
+# app.config['MQTT_TLS_ENABLED'] = False
 
+print("ok")
 mqtt = Mqtt(app)
 socketio = SocketIO(app)
-
+print("after ok")
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
     print("resource code" + str(rc))
+    print("inl connect")
     mqtt.subscribe("mytopic2", 0)
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
+    print(userdata)
     print((msg.payload).decode("utf-8"))
     print(message.payload)
     print(message.topic)
@@ -57,7 +66,9 @@ def handle_mqtt_message(client, userdata, message):
 
 @mqtt.on_log()
 def handle_logging(client, userdata, level, buf):
+    print(userdata)
     print(level, buf)
+    print("in log")
 
 @app.route('/')
 def index():
